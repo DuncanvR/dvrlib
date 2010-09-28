53a367f1691c9eba601f33b5328e83e68f21bc4a
DuncanvR <duncanvr@gmail.com>
2010-09-28 13:01:35 +0200

    Added type template to LocalSearch to get the correct type back from search().

--- src/dvrlib/localsearch/LocalSearch.java
+++ src/dvrlib/localsearch/LocalSearch.java
@@ -6,17 +6,17 @@
 
 package dvrlib.localsearch;
 
-public abstract class LocalSearch {
+public abstract class LocalSearch<P extends Problem<S>, S extends Solution> {
    /**
     * Search for a Solution for the given Problem, starting with a random Solution.
-    * @see LocalSearch#search(localsearch.Problem, localsearch.Solution)
+    * @see LocalSearch#search(dvrlib.localsearch.Problem, dvrlib.localsearch.Solution)
     */
-   public Solution search(Problem problem) {
+   public S search(P problem) {
       return search(problem, problem.randomSolution());
    }
 
    /**
     * Search for a Solution for the given Problem, starting with the given Solution.
     */
-   public abstract Solution search(Problem problem, Solution solution);
+   public abstract S search(P problem, S solution);
 }
