5716c73d772735df6493141d2f7b4600b9f848b1
DuncanvR <duncanvr@gmail.com>
2010-09-29 17:13:59 +0200

    Added equals() and hashCode() to Pair, and introduced HalfComparablePair.

--- src/dvrlib/generic/Pair.java
+++ src/dvrlib/generic/Pair.java
@@ -20,6 +20,28 @@ public class Pair<A, B> {
       this.b = b;
    }
 
+   /**
+    * Returns true if the given object is equal to this one, false otherwise.
+    * @return (obj instanceof Pair && this.a.equals(((Pair) obj).a) && this.b.equals(((Pair) obj) b))
+    * O(a.equals() + b.equals()) if the given object is a Pair, O(1) otherwise.
+    */
+   @Override
+   public boolean equals(Object obj) {
+      if(obj instanceof Pair) {
+         Pair that = (Pair) obj;
+         return (a == null ? that.a == null : a.equals(that.a)) && (b == null ? that.b == null : b.equals(that.b));
+      }
+      return false;
+   }
+
+   @Override
+   public int hashCode() {
+      int hash = 7;
+      hash = 29 * hash + (this.a == null ? 0 : this.a.hashCode());
+      hash = 29 * hash + (this.b == null ? 0 : this.b.hashCode());
+      return hash;
+   }
+
    @Override
    public String toString() {
       return "dvrlib.generic.Pair(" + a + ", " + b + ")";
