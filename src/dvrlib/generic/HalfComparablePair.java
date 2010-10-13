89e7b53dc736f1744c37592c1a7123e3bc7cd750
DuncanvR <duncanvr@gmail.com>
2010-10-13 20:59:17 +0200

    Removed equals() and hashCode() from HalfComparablePair.

--- src/dvrlib/generic/HalfComparablePair.java
+++ src/dvrlib/generic/HalfComparablePair.java
@@ -16,27 +16,6 @@ public class HalfComparablePair<A extends Comparable, B> extends Pair<A, B> impl
       super(a, b);
    }
 
-   /**
-    * Returns true if the given object is equal to this one, false otherwise.
-    * @return (obj instanceof Pair && this.a.equals(((Pair) obj).a))
-    * O(a.equals()) if the given object is a Pair, O(1) otherwise.
-    */
-   @Override
-   public boolean equals(Object obj) {
-      if(obj instanceof HalfComparablePair) {
-         HalfComparablePair that = (HalfComparablePair) obj;
-         return (a == null ? that.a == null : a.equals(that.a));
-      }
-      return false;
-   }
-
-   @Override
-   public int hashCode() {
-      int hash = 9;
-      hash = 69 * hash + (this.a == null ? 0 : this.a.hashCode());
-      return hash;
-   }
-
    public int compareTo(HalfComparablePair<A, B> that) {
       return a.compareTo(that.a);
    }
