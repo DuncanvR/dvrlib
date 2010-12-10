9a1f8e9e000d1d5063cc4dbd960a260a3e85211e
DuncanvR <duncanvr@gmail.com>
2010-12-10 21:11:45 +0100

    Added function for keeping track of the best known solution to dvrlib.localsearch.Problem.

--- src/dvrlib/localsearch/Problem.java
+++ src/dvrlib/localsearch/Problem.java
@@ -16,4 +16,15 @@ public interface Problem<S extends Solution> {
     * Returns the evaluation of the given solution, where less is better.
     */
    public double evaluate(S solution);
+
+   /**
+    * Compares the given solution to the current best, and saves it if it is good enough.
+    * Note that the solution should be copied in order to be saved, as it can (and probably will) be altered.
+    */
+   public void saveSolution(S s);
+
+   /**
+    * Returns the best solution currently known.
+    */
+   public S getBestSolution();
 }
