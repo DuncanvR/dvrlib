854822fd645b6a0e75efc75e6fcb0d34d18c8302
DuncanvR <duncanvr@gmail.com>
2012-09-12 19:01:10 +0200

    Added dvrlib.generic.ReadOnlyIterator.

---
+++ src/dvrlib/generic/ReadOnlyIterator.java
@@ -0,0 +1,32 @@
+/*
+ * DvRLib - Generic
+ * Duncan van Roermund, 2012
+ * ReadOnlyIterator.java
+ */
+
+package dvrlib.generic;
+
+import java.util.Iterator;
+
+public class ReadOnlyIterator<E> implements Iterator<E> {
+   protected final Iterator<E> it;
+
+   public ReadOnlyIterator(Iterator<E> iterator) {
+      it = iterator;
+   }
+
+   @Override
+   public boolean hasNext() {
+      return it.hasNext();
+   }
+
+   @Override
+   public E next() {
+      return it.next();
+   }
+
+   @Override
+   public void remove() {
+      throw new UnsupportedOperationException("dvrlib.generic.ReadOnlyIterator#remove() is not supported");
+   }
+}
