63468af41aa1d2c60be363f36fc0d7ae76385f7e
DuncanvR <duncanvr@gmail.com>
2013-04-06 13:03:36 +0200

    Expanded container.DisjointForestTest.

--- test-src/dvrlib/container/DisjointForestTest.java
+++ test-src/dvrlib/container/DisjointForestTest.java
@@ -6,6 +6,9 @@
 
 package dvrlib.container;
 
+import dvrlib.generic.Tuple;
+
+import java.util.Collection;
 import java.util.HashSet;
 
 import org.junit.Test;
@@ -13,9 +16,57 @@ import static org.junit.Assert.*;
 
 public class DisjointForestTest {
    @Test
-   public void testForest() {
-      Integer[][] es = new Integer[][]{{ 1, 2, 3 }, { 4, 5, 6 }, { 7, 8 }};
-      DisjointForest<Integer> df = new DisjointForest<Integer>();
+   public void testForests() {
+      testForest(new Integer[][]{{ }});
+      testForest(new Integer[][]{{ 1 }});
+      testForest(new Integer[][]{{ 1, 2 }});
+      testForest(new Integer[][]{{ 1 }, { 2 }});
+      testForest(new Integer[][]{{ 1, 2 }, { 3, 4 }});
+      testForest(new Integer[][]{{ 1, 2, 3 }});
+      testForest(new Integer[][]{{ 1, 2, 3 }, { 4, 5, 6 }});
+      testForest(new Integer[][]{{ 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }});
+      testForest(new Integer[][]{{ 1, 2 }, { 3, 4, 5, 6 }, { 7 }, { 8, 9 }});
+   }
+
+   protected void testForest(Integer[][] es) {
+      testForest(new DisjointForest<Integer>() {
+            @Override
+            public Collection<Tuple<HashSet<Integer>, Object>> retrieveSets() {
+               Collection<Tuple<HashSet<Integer>, Object>> ts = super.retrieveSets();
+               for(Tuple<HashSet<Integer>, Object> t : ts) {
+                  assertNull(t.b);
+               }
+               return ts;
+            }
+         }, es);
+
+      testForest(new AbstractDisjointForest<Integer, Integer>() {
+            @Override
+            public boolean add(Integer e) {
+               return add(e, e);
+            }
+
+            @Override
+            protected Tuple<HashSet<Integer>, Integer> merge(Tuple<HashSet<Integer>, Integer> t1, Tuple<HashSet<Integer>, Integer> t2) {
+               t1.a.addAll(t2.a);
+               t1.b += t2.b;
+               return t1;
+            }
+            public Collection<Tuple<HashSet<Integer>, Integer>> retrieveSets() {
+               Collection<Tuple<HashSet<Integer>, Integer>> ts = super.retrieveSets();
+               for(Tuple<HashSet<Integer>, Integer> t : ts) {
+                  int s = 0;
+                  for(Integer i : t.a) {
+                     s += i;
+                  }
+                  assertEquals(t.b.intValue(), s);
+               }
+               return ts;
+            }
+         }, es);
+   }
+
+   protected void testForest(AbstractDisjointForest<Integer, ?> df, Integer[][] es) {
       assertTrue(df.isEmpty());
       assertEquals(df.size(), 0);
 
@@ -26,11 +77,14 @@ public class DisjointForestTest {
             assertEquals(df.size(), ++c);
          }
       }
-      assertFalse(df.isEmpty());
+      if(c > 0)
+         assertFalse(df.isEmpty());
 
       for(int i = 0; i < es.length; i++) {
          for(int j = 1; j < es[i].length; j++) {
+            assertEquals(df.representative(es[i][j]), es[i][j]);
             df.union(es[i][0], es[i][j]);
+            assertEquals(df.representative(es[i][j]), df.representative(es[i][0]));
             assertEquals(df.size(), c);
          }
       }
@@ -40,12 +94,14 @@ public class DisjointForestTest {
             assertSetsMatch(df.retrieveSet(es[i][j]).a, es[i]);
          }
       }
+
+      df.retrieveSets();
    }
 
-   protected void assertSetsMatch(HashSet<Integer> s, Integer[] es) {
+   protected <E> void assertSetsMatch(HashSet<E> s, E[] es) {
       assertEquals(s.size(), es.length);
-      for(int i = 0; i < es.length; i++) {
-         assertTrue(s.contains(es[i]));
+      for(E e : es) {
+         assertTrue(s.contains(e));
       }
    }
 }
