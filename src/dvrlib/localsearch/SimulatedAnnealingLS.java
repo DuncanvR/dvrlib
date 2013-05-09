03130e7845f3ba2752f0c56f6182989fe961b615
DuncanvR <duncanvr@gmail.com>
2013-05-09 17:30:58 +0200

    Refactored localsearch.SimulatedAnnealingLS to retry improving the best found solution.

--- src/dvrlib/localsearch/SimulatedAnnealingLS.java
+++ src/dvrlib/localsearch/SimulatedAnnealingLS.java
@@ -17,114 +17,129 @@ public class SimulatedAnnealingLS<P extends Problem<S, E>, S extends Solution, E
       }
    }
 
+   /** The default number of retries attempted is 5. */
+   public final static int defaultRetryCount = 5;
    /** The default initial temperature is 2.4663035, so <code>dE = 1</code> results in a 2/3 chance of acceptance. */
-   public final static double defTemp    = 2.4663035;
+   public final static double defaultTemp    = 2.4663035;
    /** The default temperature modifier is 0.98, leading to a 2% decrease in temperature every <code>coolCount</code> iterations. */
-   public final static double defTempMod = 0.98;
+   public final static double defaultTempMod = 0.98;
 
    protected final Changer<P, S, ? extends Changer<P, S, ?>.Change> changer;
-   protected final int                                              stopCount,
-                                                                    coolCount;
+   protected final int                                              coolCount,
+                                                                    retryCount,
+                                                                    stopCount;
    protected final double                                           initTemp,
                                                                     tempMod;
 
    /**
     * SimulatedAnnealingLS constructor, using the default values for initTemp and tempMod.
     * @param changer   The changer that is used to make changes to the solution during the search.
-    * @param stopCount The algorithm will stop this number of iterations after the best known solution was found.
+    * @param stopCount The number of iterations after which the algorithm will retry after the best known solution was found.
     * @param coolCount The number of iterations after which the temperature will decrease.
     * @see SimulatedAnnealingLS#SimulatedAnnealingLS(Changer, int, int, double, double)
     * O(1).
     */
    public SimulatedAnnealingLS(Changer<P, S, ? extends Changer<P, S, ?>.Change> changer, int stopCount, int coolCount) {
-      this(changer, stopCount, coolCount, defTemp, defTempMod);
+      this(changer, stopCount, coolCount, defaultTemp);
    }
 
    /**
     * SimulatedAnnealingLS constructor, using the default value for tempMod.
     * @param changer   The changer that is used to make changes to the solution during the search.
-    * @param stopCount The number of iterations after which the algorithm will stop after the best known solution was found.
+    * @param stopCount The number of iterations after which the algorithm will retry after the best known solution was found.
     * @param coolCount The number of iterations after which the temperature will decrease.
     * @param initTemp  The initial temperature.
     * @see SimulatedAnnealingLS#SimulatedAnnealingLS(Changer, int, int, double, double)
     * O(1).
     */
    public SimulatedAnnealingLS(Changer<P, S, ? extends Changer<P, S, ?>.Change> changer, int stopCount, int coolCount, double initTemp) {
-      this(changer, stopCount, coolCount, initTemp, defTempMod);
+      this(changer, stopCount, coolCount, defaultRetryCount, initTemp, defaultTempMod);
    }
 
    /**
     * SimulatedAnnealingLS constructor.
-    * @param changer   The changer that is used to make changes to the solution during the search.
-    * @param stopCount The number of iterations after which the algorithm will stop after the best known solution was found.
-    * @param coolCount The number of iterations after which the temperature will decrease.
-    * @param initTemp  The initial temperature.
-    * @param tempMod   The modifier for the temperature. Every <code>coolCount</code> iterations the temperature is multiplied by this value, so its value should be in the range [0,1].
+    * @param changer    The changer that is used to make changes to the solution during the search.
+    * @param stopCount  The number of iterations after which the algorithm will retry after the best known solution was found.
+    * @param coolCount  The number of iterations after which the temperature will decrease.
+    * @param retryCount The number of retries that will be attempted.
+    * @param initTemp   The initial temperature.
+    * @param tempMod    The modifier for the temperature. Every <code>coolCount</code> iterations the temperature is multiplied by this value, so its value should be in the range [0,1].
     * O(1).
     */
-   public SimulatedAnnealingLS(Changer<P, S, ? extends Changer<P, S, ?>.Change> changer, int stopCount, int coolCount, double initTemp, double tempMod) {
-      this.changer   = changer;
-      this.stopCount = stopCount;
-      this.coolCount = coolCount;
-      this.initTemp  = initTemp;
-      this.tempMod   = tempMod;
+   public SimulatedAnnealingLS(Changer<P, S, ? extends Changer<P, S, ?>.Change> changer, int stopCount, int coolCount, int retryCount, double initTemp, double tempMod) {
+      this.changer    = changer;
+      this.stopCount  = stopCount;
+      this.coolCount  = coolCount;
+      this.retryCount = retryCount;
+      this.initTemp   = initTemp;
+      this.tempMod    = tempMod;
    }
 
    /**
     * Searches for a solution for the given problem, starting from the given solution.
     * This algorithm repeatedly generates changes for the current solution and applies them if they improve it.
     * If a change does not improve the solution, it is applied with a certain chance based on the current temperature, which decreases as time passes by.
-    * After <code>stopCount</code> iterations in which no improvements were found, the best known solution is returned.
+    * After <code>stopCount</code> iterations in which no improvements were found, all changes since the best known solution are undone, and the algorithm tries again.
+    * After <code>retryCount</code> of retries, the best know solution is returned.
     * When a solution is found that is better or equal to the given bound, the search is stopped.
     */
    @Override
    protected S doSearch(P problem, E bound, S solution) {
       SearchState state = newState(problem, solution);
-      E curEval = problem.evaluate(state), bestEval = curEval;
+      E curEval  = problem.evaluate(state),
+        bestEval = curEval;
 
-      try {
-         // Main loop: j holds the number of iterations since the last improvement
-         for(int sc = 0; sc < stopCount && !problem.betterEq(curEval, bound); sc++, state.iteration++) {
-            // Mutate the solution
-            state.changes.add(changer.makeChange(state));
-            E newEval = problem.evaluate(state);
+      for(int r = 0; r < retryCount && !problem.betterEq(bestEval, bound); r++) {
+         boolean improved = false;
+         try {
+            for(int sc = 0; sc < stopCount && !problem.betterEq(bestEval, bound); sc++, state.iteration++) {
+               // Mutate the solution
+               state.changes.add(changer.makeChange(state));
+               E newEval = problem.evaluate(state);
 
-            if(savingCriterion == LocalSearch.SavingCriterion.EveryIteration)
-               state.saveSolution();
-
-            if(problem.better(newEval, curEval)) {
-               sc = 0;
-               curEval = newEval;
-
-               if(savingCriterion == LocalSearch.SavingCriterion.EveryImprovement)
+               if(savingCriterion == LocalSearch.SavingCriterion.EveryIteration)
                   state.saveSolution();
 
-               if(problem.betterEq(newEval, bestEval)) {
-                  // If the new solution is better than the best solution found, clear the list of changes
-                  bestEval = newEval;
-                  state.changes.clear();
+               if(problem.better(newEval, curEval)) {
+                  sc = 0;
+                  curEval = newEval;
 
-                  if(savingCriterion == LocalSearch.SavingCriterion.NewBest)
+                  if(savingCriterion == LocalSearch.SavingCriterion.EveryImprovement)
                      state.saveSolution();
+
+                  // If the new solution is at least as good as the best solution found, clear the list of changes
+                  if(problem.betterEq(newEval, bestEval)) {
+                     state.changes.clear();
+
+                     if(problem.better(newEval, bestEval)) {
+                        improved = true;
+                        bestEval = newEval;
+                        if(savingCriterion == LocalSearch.SavingCriterion.NewBest)
+                           state.saveSolution();
+                     }
+                  }
                }
-            }
-            else if(Math.random() < Math.exp((newEval.doubleValue() - curEval.doubleValue()) * LocalSearch.asNumber(problem.direction()) / state.temperature)) {
-               // Accept the change, even though it's not an improvement
-               curEval = newEval;
-            }
-            else
-               state.changes.undoLast(state);
+               else if(Math.random() < Math.exp((newEval.doubleValue() - curEval.doubleValue()) * LocalSearch.asNumber(problem.direction()) / state.temperature)) {
+                  // Accept the change, even though it's not an improvement
+                  curEval = newEval;
+               }
+               else
+                  state.changes.undoLast(state);
 
-            // Decrease the temperature according to the schedule
-            if(state.iteration % coolCount == 0)
-               state.temperature *= tempMod;
+               // Decrease the temperature according to the schedule
+               if(state.iteration % coolCount == 0)
+                  state.temperature *= tempMod;
+            }
          }
-      }
-      catch(CannotChangeException _) { }
+         catch(CannotChangeException _) { }
 
-      // Undo the changes since the last improvement, reverting to the best known solution
-      state.iteration -= state.changes.size();
-      state.changes.undoAll(state);
+         // Undo the changes since the last improvement, reverting to the best known solution
+         state.iteration -= state.changes.size();
+         state.changes.undoAll(state);
+
+         if(improved)
+            r = -1;
+      }
 
       state.saveSolution();
       return state.solution();
