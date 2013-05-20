98aa18aca397b6a961aa9c12444759d830b57f2e
DuncanvR <duncanvr@gmail.com>
2013-05-20 12:27:37 +0200

    Introduced generic.Util#pluralise(int, String) and pluraliseInc(int, String).

--- src/dvrlib/generic/Util.java
+++ src/dvrlib/generic/Util.java
@@ -14,6 +14,7 @@ public class Util {
     * @param lowerBound The minimum value of the returned number.
     * @param upperBound The maximum value of the returned number.
     * @param msg        The message that will be printed before the prompt.
+    * @return The number that was read from standard input.
     */
    public static int readNumber(int lowerBound, int upperBound, String msg) {
       if(lowerBound > upperBound)
@@ -30,5 +31,27 @@ public class Util {
          catch(java.io.IOException | java.util.InputMismatchException _) { }
          System.out.println("Invalid number entered");
       }
+   }
+
+   /**
+    * E.g. <code>Util.pluralise(0, "apple")</code> returns <code>"apples"</code>, but <code>Util.pluralise(1, "pear")</code> returns "pear".
+    * @param num  The number to use.
+    * @param word The word to pluralise.
+    * @return The pluralised word.
+    */
+   public static String pluralise(int num, String word) {
+      return (num == 1 ? word : word + "s");
+   }
+
+   /**
+    * Returns the given word, pluralised by adding a postfix 's' if applicable, and prefixed by the given number.
+    * This is a convenience function that returns <code>num + " " + pluralise(num, word)</code>.
+    * @param num  The number to use and include.
+    * @param word The word to pluralise.
+    * @return A string containing the number and the pluralised word.
+    * @see Util#pluralise(int, String)
+    */
+   public static String pluraliseInc(int num, String word) {
+      return num + " " + pluralise(num, word);
    }
 }
